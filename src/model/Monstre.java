28d0cd8495a7d06812626ce6d36a005f97ee7818
Nianfo <snianfo@gmail.com>
2016-11-22 23:12:44 +0100

    Edif les classes heros, monstre et element

--- src/model/Monstre.java
+++ src/model/Monstre.java
@@ -2,4 +2,8 @@ package model;
 
 public class Monstre extends Soldat{
 
+	public Monstre(Grille carte, TypesH type ,String nom, Position pos){
+		//super(carte,type,nom,pos);
+		this.couleur=COULEUR_MONSTRES;
+	}
 }
