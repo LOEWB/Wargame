f16579dd093d175b74c8409aacfed1a2237ee3bc
Benny <benoitloew@yahoo.fr>
2016-12-03 15:50:37 +0100

    fix: affichage du nom et de la vie au survol des soldats

--- src/view/Footer.java
+++ src/view/Footer.java
@@ -11,6 +11,7 @@ package view;
      public class Footer extends JPanel {
          private JLabel labelPosition;
          private JLabel labelRace;
+         private JLabel iconCoeur;
 
              public Footer(ControllerFinTour controller) {
                  labelPosition = new JLabel();
@@ -18,9 +19,24 @@ package view;
                  labelRace = new JLabel();
                  labelRace.setPreferredSize(new Dimension(200,20));
 
-                 this.setLayout(new GridLayout(1,3));
-                 this.add(labelPosition);
-                 this.add(labelRace);
+                 java.net.URL imgURL = getClass().getResource("/ressources/heart.png");
+                 if (imgURL != null) {
+                     iconCoeur = new JLabel(new ImageIcon(imgURL));
+                 } else {
+                     System.err.println("Couldn't find file: " + "/ressources/heart.png");
+                 }
+                 iconCoeur.setPreferredSize(new Dimension(200,20));
+
+                 GridBagLayout layout = new GridBagLayout();
+                 GridBagConstraints c = new GridBagConstraints();
+                 c.gridheight=1;
+
+                 this.setLayout(layout);
+                 this.add(labelPosition,c);
+                 this.add(labelRace,c);
+                 this.add(iconCoeur,c);
+                 iconCoeur.setVisible(false);
+
 
          }
 
@@ -30,11 +46,13 @@ package view;
                  {
                      Soldat soldatCourant = (Soldat) caseModel.getElement();
                      labelRace.setText(soldatCourant.getType()+" "+soldatCourant.getVie());
+                     iconCoeur.setVisible(true);
                  }
          }
 
              public void notifierSortieCase(){
                  labelPosition.setText("");
                  labelRace.setText("");
+                 iconCoeur.setVisible(false);
          }
  }
